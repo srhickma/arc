54613dad967aab9385e65870fab8fdcc2daf7d40
Shane Hickman <shanerhickman@gmail.com>
2026-02-26 20:01:58 -0800

    Config file + reading, writing, and comparing checksums

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"hash"
 	"io"
@@ -13,12 +14,18 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
+)
+
+const (
+	sumFile  = "arc.sum"
+	confFile = "arc.conf"
 )
 
 type Config struct {
-	hashType string
-	folder   string
-	workers  int
+	HashType string `json:"hashType"`
+	Folder   string `json:"folder"`
+	Workers  int    `json:"workers"`
 }
 
 type hasher struct {
@@ -63,27 +70,49 @@ type fileHandle struct {
 	info fs.FileInfo
 }
 
+type fileInfo struct {
+	Checksum string    `json:"checksum"`
+	ModTime  time.Time `json:"modTime"`
+}
+
 func main() {
 	args := os.Args[1:]
 	for _, arg := range args {
 		fmt.Println(arg)
 	}
 
-	config := &Config{
-		hashType: "sha256",
-		folder:   "/mnt/vat/photos",
-		workers:  8,
+	configData, err := os.ReadFile(confFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var config Config
+	if err := json.Unmarshal(configData, &config); err != nil {
+		log.Fatal(err)
 	}
 
-	hasher, err := newHasher(config.hashType)
+	oldSums := make(map[string]fileInfo)
+	sumPath := filepath.Join(config.Folder, sumFile)
+	if bytes, err := os.ReadFile(sumPath); err == nil {
+		if err := json.Unmarshal(bytes, &oldSums); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	hasher, err := newHasher(config.HashType)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	wg := sync.WaitGroup{}
-	fhChan := make(chan fileHandle, config.workers)
+	var lock sync.Mutex
+	newSums := make(map[string]fileInfo)
+	var totalBytes int64
 
-	for range config.workers {
+	wg := sync.WaitGroup{}
+	fhChan := make(chan fileHandle, config.Workers)
+
+	startTime := time.Now()
+
+	for range config.Workers {
 		wg.Go(func() {
 			for {
 				fh, ok := <-fhChan
@@ -95,27 +124,36 @@ func main() {
 				if err != nil {
 					log.Fatal(err) // TODO propagate the error instead
 				}
-				fmt.Printf("%s - %s\n", checksum, fh.path)
+
+				lock.Lock()
+				newSums[fh.path] = fileInfo{
+					Checksum: checksum,
+					ModTime:  fh.info.ModTime(),
+				}
+				totalBytes += fh.info.Size()
+				lock.Unlock()
 			}
 		})
 	}
 
-	err = filepath.WalkDir(config.folder, func(path string, entry fs.DirEntry, err error) error {
+	err = filepath.WalkDir(config.Folder, func(path string, entry fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if !entry.IsDir() {
-			info, err := entry.Info()
-			if err != nil {
-				return err
-			}
+		if entry.IsDir() || entry.Name() == sumFile {
+			return nil
+		}
 
-			fhChan <- fileHandle{
-				path: path,
-				name: entry.Name(),
-				info: info,
-			}
+		info, err := entry.Info()
+		if err != nil {
+			return err
+		}
+
+		fhChan <- fileHandle{
+			path: path,
+			name: entry.Name(),
+			info: info,
 		}
 
 		return nil
@@ -126,4 +164,52 @@ func main() {
 
 	close(fhChan)
 	wg.Wait()
+
+	fmt.Printf(
+		"computed %d checksums over %.2fMB in %s\n",
+		len(newSums),
+		float64(totalBytes)/(1024*1024),
+		time.Since(startTime).Round(time.Millisecond),
+	)
+
+	changed := false
+	for path, info := range newSums {
+		prev, ok := oldSums[path]
+		if !ok {
+			fmt.Printf("ADDED: %s\n", path)
+			changed = true
+		} else if prev.Checksum != info.Checksum {
+			fmt.Printf("CHANGED: %s\n", path)
+			changed = true
+		}
+	}
+
+	for path := range oldSums {
+		if _, ok := newSums[path]; !ok {
+			fmt.Printf("REMOVED: %s\n", path)
+			changed = true
+		}
+	}
+
+	if !changed {
+		fmt.Println("no changes detected")
+		return
+	}
+
+	fmt.Printf("write new checksums to %s? [y/N]: ", sumFile)
+	var response string
+	fmt.Scanln(&response)
+	if response != "y" && response != "Y" {
+		fmt.Println("skipping write")
+		return
+	}
+
+	data, err := json.MarshalIndent(newSums, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := os.WriteFile(sumPath, data, 0644); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("wrote %d checksums to %s\n", len(newSums), sumFile)
 }
